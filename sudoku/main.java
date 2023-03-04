48197e1d8f6d1777f813007d906aa64e96f050f8
mukhammad aziz <inobiddinovmuhammadaziz@gmail.com>
2023-03-04 23:25:30 +0800

    added

---
+++ sudoku/main.java
@@ -0,0 +1,11 @@
+public class main{
+
+public static void main(String[]argc){
+int arr[][] = new int[9][9];
+for(int a = 0; a < arr.length;a++){
+System.out.print("	"+arr[a][1]);
+System.out.print("	"+arr[a][1]);
+System.out.print("	"+arr[a][1]);
+}
+}
+}
