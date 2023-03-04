c9e3f1052af99e198452ea9e7af4edc359c9edbe
mukhammad aziz <inobiddinovmuhammadaziz@gmail.com>
2023-03-04 22:47:16 +0800

    done

---
+++ helloWorld.java
@@ -0,0 +1,15 @@
+public class helloWorld{
+
+public static void main(String[]argc)
+{
+int a;
+a = 0;
+for(int b = 0;b< 15;b++){
+System.out.printf("%d %d\n",a,b);
+a+=2;
+	
+}
+System.out.println("Hello");	
+
+}
+}
